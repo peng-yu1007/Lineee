12280393e65cfda706b39fbb67bf4e110a60070f
peng-yu1007 <alice93019@gmail.com>
2021-12-09 18:36:58 +0800

    新光醫院1

---
+++ Lineee/Controllers/DoctorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lineee.Models;
+
+namespace Lineee.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private LineNotify1Entities db = new LineNotify1Entities();
+        // GET: PatientList
+        public ActionResult Index()
+        {
+            return View(db.Patient.ToList());
+        }
+    }
+}
