8f46093f6232f14ae8334cdba0a8622a11288c02
peng-yu1007 <alice93019@gmail.com>
2022-01-04 00:57:17 +0800

    傳送line訊息

--- Lineee/Controllers/DoctorController.cs
+++ Lineee/Controllers/DoctorController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using Lineee.Models;
+using RestSharp;
 
 namespace Lineee.Controllers
 {
@@ -13,6 +15,51 @@ namespace Lineee.Controllers
         // GET: PatientList
         public ActionResult Index()
         {
+
+            //HttpContent stringContent1 = new StringContent("msg");
+            //using (var content = new MultipartFormDataContent())
+            //{
+            //    content.Add(stringContent1, "QAQ");
+
+            //    var req = new HttpClient();
+            //    var test = req.PostAsync($"https://script.google.com/macros/s/AKfycbyB2u5E72rhN3YcBzjDravC7wgMp1M-DK1ZYpoIkt10jAKafj-rZ-t7tAB8TXsr4TM/exec", content);
+            //}
+
+
+            //// Définition des variables qui seront envoyés
+            //HttpContent stringContent1 = new StringContent("msg"); // Le contenu du paramètre P1
+            ////HttpContent bytesContent = new ByteArrayContent(paramFileBytes);
+
+            //using (var client = new HttpClient())
+            //using (var formData = new MultipartFormDataContent())
+            //{
+            //    formData.Add(stringContent1, "QAQ"); // Le paramètre P1 aura la valeur contenue dans param1String
+
+            //    //  formData.Add(bytesContent, "file2", "file2");
+            //    try
+            //    {
+            //        var response = client.PostAsync($"https://script.google.com/macros/s/AKfycbyB2u5E72rhN3YcBzjDravC7wgMp1M-DK1ZYpoIkt10jAKafj-rZ-t7tAB8TXsr4TM/exec", formData).Result;
+
+            //    }
+            //    catch (Exception Error)
+            //    {
+
+            //    }
+            //    finally
+            //    {
+            //        client.CancelPendingRequests();
+            //    }
+            //}
+
+            // post 
+            //var client = new RestClient("https://script.google.com/macros/s/AKfycbyB2u5E72rhN3YcBzjDravC7wgMp1M-DK1ZYpoIkt10jAKafj-rZ-t7tAB8TXsr4TM/exec");
+            //client.Timeout = -1;
+            //var request = new RestRequest(Method.POST);
+            //request.AlwaysMultipartFormData = true;
+            //request.AddParameter("msg", "QAQ");
+            //IRestResponse response = client.Execute(request);
+
+
             return View(db.Patient.ToList());
         }
 
--- Lineee/Controllers/ReportsController.cs
+++ Lineee/Controllers/ReportsController.cs
@@ -4,9 +4,13 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Lineee.Models;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Lineee.Controllers
 {
@@ -124,6 +128,70 @@ namespace Lineee.Controllers
             Report report = db.Report.Find(id);
             db.Report.Remove(report);
             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // POST: Reports/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        public ActionResult SendLine([Bind(Include = "test")] string test)
+        {
+
+            var teststr = test.ToString();
+
+            //      $.post('https://script.google.com/macros/s/AKfycbyB2u5E72rhN3YcBzjDravC7wgMp1M-DK1ZYpoIkt10jAKafj-rZ-t7tAB8TXsr4TM/exec',
+            //    { msg: '哈囉!!!' },
+            //    function(e) {
+            //    console.log(e);
+            //});
+
+
+            //content.Add(new StringContent("key1"), "value1", "file1.txt");
+            //content.Add(new StringContent("key2"), "value2", "file2.txt");
+
+            //var req = new HttpClient();
+            //req.PostAsync("http://localhost:50640/api/multipart", content).Wait();
+
+            var client = new RestClient("https://script.google.com/macros/s/AKfycbyB2u5E72rhN3YcBzjDravC7wgMp1M-DK1ZYpoIkt10jAKafj-rZ-t7tAB8TXsr4TM/exec");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AlwaysMultipartFormData = true;
+            request.AddParameter("msg", test);
+            IRestResponse response = client.Execute(request);
+
+            //using (var httpClient = new HttpClient())
+            //{
+            //    // 改Form-data
+            //    string param = JsonConvert.SerializeObject(new { msg = test });
+            //    var contentPost = new StringContent(param, Encoding.UTF8, "multipart/form-data");
+            //    var path = new Uri($"https://script.google.com/macros/s/AKfycbyB2u5E72rhN3YcBzjDravC7wgMp1M-DK1ZYpoIkt10jAKafj-rZ-t7tAB8TXsr4TM/exec");
+            //    var result = httpClient.PostAsync(path, contentPost).Result;
+
+            //    if (result != null)
+            //    {
+            //        if (!result.IsSuccessStatusCode)
+            //        {
+            //            string message = $"失敗 回應:{result.ReasonPhrase}";
+
+            //        }
+
+            //        // 200成功
+            //        if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            //        {
+            //            var resultResponseJs = result.Content.ReadAsStringAsync();
+            //        }
+            //        else
+            //        {
+
+            //        }
+            //    }
+            //    else
+            //    {
+
+            //    }
+            //}
+
             return RedirectToAction("Index");
         }
 
