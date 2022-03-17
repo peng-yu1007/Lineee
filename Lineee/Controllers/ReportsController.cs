70ef3e2dedebbb741534743af4ff7be72d35adc9
peng-yu1007 <alice93019@gmail.com>
2022-03-17 15:43:11 +0800

    顯示網址

--- Lineee/Controllers/ReportsController.cs
+++ Lineee/Controllers/ReportsController.cs
@@ -205,44 +205,42 @@ namespace Lineee.Controllers
         [HttpPost]
         public ActionResult Decrypt(Report report)
         {
-            if (ModelState.IsValid)
+            string Decrypt()
             {
-                string Decrypt()
+                try
                 {
-                    try
+                    string textToDecrypt = report.text;
+                    string CryptoKey = report.key;
+                    string decrypt = "";
+
+                    AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                    MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                    SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+                    byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                    byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
+                    aes.Key = key;
+                    aes.IV = iv;
+
+                    byte[] dataByteArray = Convert.FromBase64String(textToDecrypt);
+                    using (MemoryStream ms = new MemoryStream())
                     {
-                        string textToDecrypt =report.text;
-                        string CryptoKey = report.key;
-                        string decrypt = "";
-
-                        AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
-                        MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-                        SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
-                        byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
-                        byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(CryptoKey));
-                        aes.Key = key;
-                        aes.IV = iv;
-
-                        byte[] dataByteArray = Convert.FromBase64String(textToDecrypt);
-                        using (MemoryStream ms = new MemoryStream())  
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
                         {
-                            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
-                            {
-                                cs.Write(dataByteArray, 0, dataByteArray.Length);
-                                cs.FlushFinalBlock();
-                                decrypt = Encoding.UTF8.GetString(ms.ToArray());
-                                return decrypt;
-                            }
+                            cs.Write(dataByteArray, 0, dataByteArray.Length);
+                            cs.FlushFinalBlock();
+                            decrypt = Encoding.UTF8.GetString(ms.ToArray());
+                            return decrypt;
                         }
                     }
-                    catch (Exception e)
-                    {
-                        throw new Exception(e.Message, e.InnerException);
-                    }
-                   
-                }  Decrypt();
-            }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message, e.InnerException);
+                }
 
+            }
+            //Decrypt();
+            ViewBag.address = Decrypt();
             return View("Secret");
         }
 
