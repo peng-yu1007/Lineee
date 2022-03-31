54e76357e78dcf8324a6659950ed35a7f3441b9b
peng-yu1007 <alice93019@gmail.com>
2022-03-31 17:48:38 +0800

    linebot開頭

---
+++ Lineee/Controllers/LineMessagesSampleController.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Lineee.Controllers
+{
+    [RoutePrefix("line")]
+    public class LineMessagesSampleController : ApiController
+    {
+        [HttpGet]
+        [Route]
+        public async Task<HttpResponseMessage> Post(HttpRequestMessage request)
+        {   
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello LINE BOT");
+
+
+
+        }
+    }
+}
---
+++ Lineee/Controllers/Startup1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Lineee.Controllers.Startup1))]
+
+namespace Lineee.Controllers
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 如需如何設定應用程式的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkID=316888
+
+            // Wire Web API
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
